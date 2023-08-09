16f31add5538e4098628a980de6a26fa51c9d9a0
kitou88C39 <kanfutrooper@icloud.com>
2023-08-09 09:45:20 +0900

    first commit

---
+++ src/main/java/com/dsousa/minhasfinans/MinhasfinansApplication.java
@@ -0,0 +1,13 @@
+package com.dsousa.minhasfinans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasfinansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasfinansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsousa/minhasfinans/MinhasfinansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsousa.minhasfinans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
